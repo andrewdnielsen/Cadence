e3007f46086efe183923abd57d83c7e3f60b5d97
andrewdnielsen <andrewdnielsen@gmail.com>
2025-07-03 17:42:55 -0500

    Initial Commit

---
+++ Cadence/CadenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadenceApp.swift
+//  Cadence
+//
+//  Created by Andrew Nielsen on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CadenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
