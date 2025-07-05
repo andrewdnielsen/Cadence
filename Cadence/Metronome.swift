5150025f412ee712a8471eca3ff8c599863d07f2
andrewdnielsen <andrewdnielsen@gmail.com>
2025-07-05 12:21:22 -0500

    Implemented the init() method for the metronome class, connected all AudioKit nodes

--- Cadence/Metronome.swift
+++ Cadence/Metronome.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import AudioKit
+import AVFoundation
 
 struct TimeSignature {
     var beats: Int = 4
@@ -22,4 +23,53 @@ class Metronome {
     let player = AudioPlayer()
     let sequencer = AppleSequencer()
     var callbackInstrument: MIDICallbackInstrument!
+    
+    init() {
+        // Defining what happens on each beat by creating a MIDICallbackInstrument
+        callbackInstrument = MIDICallbackInstrument { [weak self] status, note, velocity in
+            
+            // Creating a MIDIstatus object and checking if it is .noteOn, otherwise do nothing
+            guard let midiStatus = MIDIStatus(byte: status), midiStatus.type == .noteOn else { return }
+            
+            // Playing the sound if it is a valid noteOn event
+            self?.player.play()
+        }
+        
+        // Loading in the metronome click sound
+        
+        if let fileURL = Bundle.main.url(forResource: "click", withExtension: "wav") {
+            do {
+                try player.load(url: fileURL)
+            } catch {
+                print("Error loading audio file: \(error)")
+            }
+        } else {
+            print("Could not find click.wav")
+        }
+        
+        
+        // Connect player to engine's output
+        engine.output = player
+        
+        
+        // Creating sequence of beats
+        let track = sequencer.newTrack()
+        
+        // Setting up the track to play the callbackInstrument
+        track?.setMIDIOutput(callbackInstrument.midiIn)
+        
+        // Adding a note to the track for each beat defined in our time signature
+        
+        for beat in 0..<timeSignature.beats{
+            track?.add(noteNumber: MIDINoteNumber(60),
+                       velocity: 127,
+                       position: Duration(beats: Double(beat)),
+                       duration: Duration(beats: 0.1))
+        }
+        
+        // Setting sequencer to match time signature
+        sequencer.setLength(Duration(beats: Double(timeSignature.beats)))
+        sequencer.enableLooping()
+        sequencer.setTempo(bpm)
+    }
 }
